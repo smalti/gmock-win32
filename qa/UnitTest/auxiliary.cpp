84afdb51dddd3bf087b08e28385a43eca67acb71
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2024-01-04 00:21:50 +0300

    examples

---
+++ qa/UnitTest/auxiliary.cpp
@@ -0,0 +1,30 @@
+﻿#include "gtest\gtest.h"
+#include "gmock\gmock.h"
+#include "gmock-win32.h"
+#include <Windows.h>
+using ::testing::Return;
+
+MOCK_STDCALL_FUNC(DWORD, GetCurrentProcessId);
+
+TEST(auxiliary, invoke_real_wrong) {
+	EXPECT_EQ( nullptr, REAL_MODULE_FUNC( GetCurrentProcessId ) );
+	EXPECT_ANY_THROW( INVOKE_REAL_MODULE_FUNC( GetCurrentProcessId ) );
+}
+TEST(auxiliary, invoke_real_correct) {
+	EXPECT_MODULE_FUNC_CALL( GetCurrentProcessId ).WillOnce( Return( 0 ) );
+	EXPECT_EQ( ::GetCurrentProcessId( ), 0 );
+	EXPECT_NE( INVOKE_REAL_MODULE_FUNC( GetCurrentProcessId ), 0 );
+}
+TEST(auxiliary, restore) {
+	EXPECT_MODULE_FUNC_CALL( GetCurrentProcessId ).WillOnce( Return( 0 ) );
+	EXPECT_EQ( ::GetCurrentProcessId( ), 0 );
+	RESTORE_MODULE_FUNC( GetCurrentProcessId );
+	EXPECT_NE( ::GetCurrentProcessId( ), 0 );
+}
+TEST(auxiliary, restore_then_expect_call) {
+	EXPECT_MODULE_FUNC_CALL( GetCurrentProcessId ).WillOnce( Return( 0 ) );
+	EXPECT_EQ( ::GetCurrentProcessId( ), 0 );
+	RESTORE_MODULE_FUNC( GetCurrentProcessId );
+	EXPECT_MODULE_FUNC_CALL( GetCurrentProcessId ).WillOnce( Return( 0 ) );
+	EXPECT_EQ( ::GetCurrentProcessId( ), 0 );
+}
---
+++ qa/UnitTest/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "gtest\gtest.h"
+#include "gmock\gmock.h"
+#include "gmock-win32.h"
+#include <Windows.h>
+
+int main(int argc, char **argv) {
+	const gmock_win32::init_scope gmockWin32{ };
+	::testing::InitGoogleTest( &argc, argv );
+	std::cout << "Running main() from " << __FILE__ << std::endl;
+	return RUN_ALL_TESTS( );
+}
