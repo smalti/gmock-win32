e91d343d00a1e432a602cf333db3b07abaf6e89b
Tymofii Smirnov <t_smirnov@wargaming.net>
2024-01-23 22:10:22 +0200

    minor change: added missed `noexcept` specifier

--- src/gmock-win32.cpp
+++ src/gmock-win32.cpp
@@ -65,7 +65,7 @@ namespace utils {
     }
 #pragma warning(pop)
 
-    int strcmp(const char* s1, const char* s2)
+    int strcmp(const char* s1, const char* s2) noexcept
     {
         while (*s1 && (*s1 == *s2))
             s1++, s2++;
